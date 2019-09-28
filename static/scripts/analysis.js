74c7d0a42bc05943600376b9523f40476dfbb7e3
Supriyo Mitra <smitra@artinfo.com>
2019-09-29 03:06:40 +0530

    Implemented market cap plot of Invest Data DB

--- static/scripts/analysis.js
+++ static/scripts/analysis.js
@@ -2,44 +2,6 @@ function showcoinbase(){
 }
 
 
-function displayplots(indexval, urlprefix){
-    selectbox = document.getElementById(indexval);
-    selectedvalue = selectbox.value;
-    //alert(indexval);
-    //alert(selectedvalue);
-    postdata = "csrfmiddlewaretoken=" + document.cryptoanalysis.csrfmiddlewaretoken.value;
-    plotdiv = document.getElementById('plotimage');
-    var xmlhttp;
-    if (window.XMLHttpRequest){
-        xmlhttp=new XMLHttpRequest();
-    }
-    else{
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.readyState == 4 && xmlhttp.status==200){
-	    plotdiv.style.display = "block";
-	    //plotdiv.innerHTML = "";
-	    waittag = document.getElementById('waitprocess');
- 	    waittag.parentNode.removeChild(waittag);
-	    imgtag = document.createElement('img');
-	    imgtag.setAttribute('src', xmlhttp.responseText);
-	    plotdiv.appendChild(imgtag);
-        }
-    };
-    posturl = urlprefix + "/cryptocurry/analyze/visual/" + indexval + "/" + selectedvalue + "/";
-    xmlhttp.open('POST', posturl, true);
-    xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    xmlhttp.setRequestHeader("X-CSRFToken", document.cryptoanalysis.csrfmiddlewaretoken.value);
-    //alert(posturl);
-    plotdiv.style.display = "block";
-    imgtag = document.createElement('img');
-    imgtag.setAttribute('src', 'static/images/loading_big.gif');
-    imgtag.setAttribute('id', 'waitprocess');
-    imgtag.setAttribute('title', 'This may take a while depending on the amount of data it needs to crunch. Please be patient.');
-    plotdiv.appendChild(imgtag);
-    xmlhttp.send(postdata);
-}
 
 
 function showloginscreen(urlprefix){
@@ -104,7 +66,6 @@ function dologin(urlprefix){
     logindiv.style.display = "";
     //alert(data);
     xmlhttp.send(data);
-
 }
 
 
